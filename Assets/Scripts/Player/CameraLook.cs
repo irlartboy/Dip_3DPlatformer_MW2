f5ed2ea893ae750357c97a7b574d309a149d873f
irlartboy <ollie_king99@outlook.com>
2019-08-27 17:53:36 +1000

    Movement

--- Assets/Scripts/Player/CameraLook.cs
+++ Assets/Scripts/Player/CameraLook.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool isCursorHidden = true;
+    public float minPitch = -60f, maxPitch = 80f;
+    public Vector2 speed = new Vector2(120f, 120f);
+
+
+    private Vector2 euler; // current rotation of camera
+
     void Start()
     {
-        
+        if (isCursorHidden)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        euler = transform.eulerAngles;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        euler.y += Input.GetAxis("Mouse X") * speed.x * Time.deltaTime;
+        euler.x -= Input.GetAxis("Mouse Y") * speed.y * Time.deltaTime;
+
+        // Clamp 
+        euler.x = Mathf.Clamp(euler.x, minPitch, maxPitch);
+        // rotate player and camera seperate
+        transform.parent.localEulerAngles = new Vector3(0, euler.y, 0);
+        transform.localEulerAngles = new Vector3(euler.x, 0, 0);
     }
 }
--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,33 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
-
+    [Header("UI")]
     public Slider healthSlider;
     public Image healthFill;
 
     public Slider jetpackSlider;
     public Image jetpackFill;
 
-    public float speed;
+    public GameObject gameOverPanel;
+    public GameObject gameUIPanel;
+
+    [Header("Player Stats")]
     public float curHealth;
-    public float maxHealth;
+    public float health;
 
     public float curJetpack;
-    public float maxJetpack;
+    public float jetpack;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        healthSlider.value = Mathf.Clamp01(curHealth / health);
+        jetpackSlider.value = Mathf.Clamp01(curJetpack / jetpack);
+    }
+    public void PlayerTakeDamage(int damage)
+    {
+        //health -= damage;
+        curHealth -= damage;
+        if (curHealth <= 0)
+        {
+            Destroy(gameObject);
+            gameOverPanel.SetActive(true);
+            gameUIPanel.SetActive(false);
+            healthSlider.enabled = false;
+
+        }
+    }
+    void ManageHealthBar()
+    {
+        if (curHealth <= 0 && healthFill.enabled)
+        {
+            healthFill.enabled = false;
+        }
     }
 }
