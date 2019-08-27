0d2ca4e9534f88d187dd9d3b466cea7c2416ab66
irlartboy <ollie_king99@outlook.com>
2019-08-27 12:44:51 +1000

    Greyboxing

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public Slider healthSlider;
+    public Image healthFill;
+
+    public Slider jetpackSlider;
+    public Image jetpackFill;
+
+    public float speed;
+    public float curHealth;
+    public float maxHealth;
+
+    public float curJetpack;
+    public float maxJetpack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
