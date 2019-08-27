3563727ff2a738d17644ef7b45021b67b01ce367
irlartboy <ollie_king99@outlook.com>
2019-08-27 12:58:48 +1000

    update

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject playPanel;
+    public GameObject optionsPanel;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);
+    }
+
+    public void OptionsButton()
+    {
+        playPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void BackButton()
+    {
+        optionsPanel.SetActive(false);
+        playPanel.SetActive(true);
+    }
+
+
+    
+}
