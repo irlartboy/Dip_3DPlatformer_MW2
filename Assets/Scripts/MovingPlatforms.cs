b25db223c285b246b98639748da4c79de87be69d
irlartboy <ollie_king99@outlook.com>
2019-09-24 09:33:10 +1000

    update

---
+++ Assets/Scripts/MovingPlatforms.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour
+{
+    public Transform[] Waypoints;
+    public float speed = 2f;
+    public int CurrentPoint = 0;
+
+    void Update()
+    {
+        if (transform.position.y != Waypoints[CurrentPoint].transform.position.y)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Waypoints[CurrentPoint].transform.position, speed * Time.deltaTime);
+        }
+    }
+}
