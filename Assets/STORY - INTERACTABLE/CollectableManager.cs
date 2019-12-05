6c4a72fc8f0f4510d10d9055300442eb3235db3a
BaileeVaughan <47824584+BaileeVaughan@users.noreply.github.com>
2019-12-05 12:01:02 +1100

    Added Interactable story elements to the scene

---
+++ Assets/STORY - INTERACTABLE/CollectableManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectableManager : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public Text logTitle, logDesc;
+    public GameObject device;
+    public float playerReach = 1f;
+
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        Cursor.visible = false;
+        device.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact")) //CHANGE Fire1 TO Interact IN AXIS SETTINGS FOR THIS TO WORK
+        {
+            Ray interact;
+            interact = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+            RaycastHit hitInfo;
+            if (Physics.Raycast(interact, out hitInfo, playerReach))
+            {
+                if (hitInfo.collider.CompareTag("Collectable")) //ITEMS MUST BE TAGGED "Collectable"
+                {
+                    Time.timeScale = 0;
+                    Cursor.visible = true;
+                    device.SetActive(true);
+                    logTitle.text = hitInfo.collider.GetComponent<DeviceInfo>().logTitle;
+                    logDesc.text = hitInfo.collider.GetComponent<DeviceInfo>().logDesc;
+                }
+            }
+        }
+
+        if (device)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 1;
+                Cursor.visible = false;
+                device.SetActive(false);
+                logTitle.text = "";
+                logDesc.text = "";
+            }
+
+        }
+    }
+}
---
+++ Assets/STORY - INTERACTABLE/DeviceInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceInfo : MonoBehaviour
+{
+    [TextArea(1, 2)]
+    public string logTitle;
+    [TextArea(5, 15)]
+    public string logDesc;
+}
